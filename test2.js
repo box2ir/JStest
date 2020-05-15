a1a653ac6b6119a1d77933fbce1976d2af0fc71f
Alex <karimovazo@yandex.ru>
2020-05-15 15:48:28 +0500

    Третий коммит

---
+++ test2.js
@@ -0,0 +1,58 @@
+'use strict';
+
+// var number = 5;
+// var string = "Hello";
+// var sym = Symbol();
+// var boolean = true;
+
+
+// var obj = {};
+
+// console.log(4/0);
+// console.log('string'*8);
+
+// let some1;
+// console.log(some1);
+
+// let persone = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(persone.name);
+// console.log(persone["age"]);
+// let a;
+// console.log(a);
+
+// console.log(typeof(String(6)));
+
+// console.log("err " + false);
+
+// console.log(typeof(Number('4')));
+
+// console.log(5 + + '5');
+
+// console.log(parseInt('15px', 10));
+
+
+
+// alert("Hello!!!");
+
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+// let numberFruit = prompt("Write number from 0 to 2", "0");
+// let fruit = ['banana', 'apple', 'lemon'];
+// console.log("Массив это типа - " + typeof(fruit));
+// console.log("null это типа - " + typeof(null));
+// console.log("undefined это типа - " + typeof(undefined));
+
+// console.log(typeof(+numberFruit));
+
+// let incr = 10,
+//     decr = 10;
+// console.log(incr++);
+// console.log(decr--);
+// console.log(incr);
+// console.log(decr);
+
